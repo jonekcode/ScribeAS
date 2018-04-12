2e45803db3e40dffe6d3dd35b16572ec18a9020c
Rohan Burke <coolq1000@gmail.com>
2018-04-12 23:30:55 +1000

    Changed the table.print function inside LuaExtensions.cs

--- ScribeBot/Wrappers/LuaExtensions.cs
+++ ScribeBot/Wrappers/LuaExtensions.cs
@@ -18,13 +18,48 @@ namespace ScribeBot.Wrappers
                 repeat until os.clock() > callTime
             end
 
-            function table.print(t)
-                print( '{' )
+            --[[
+            TABLE.PRINT(TABLE):
+                - Takes one argument (other two are for internal use).
+                - Recursively pretty prints a table, formatted with 'INDEX: [TYPE] VALUE'
+            ]]
+            function table.print(t, iteration, t_colon)
+            
+                -- Variable setup,
+                local iteration = iteration or 0
+                local t_colon = t_colon or ''
+                local count = 1
+                
+                -- Print initial `{`,
+                print(string.format('%' .. iteration * 2 .. 's{', ''))
+                
+                -- Loop through every element and print it accordingly,
                 for key, value in pairs(t) do
-                    print(string.format('\t%-20s= %s', tostring(key), tostring(value)))
+                    -- Test weather this is the last element, if so, remove colon,
+                    local colon = ','
+                    if count == #t then
+                    colon = ''
+                    end
+                    
+                    -- Check types, and print it accordingly,
+                    if type(value) == 'table' then
+                    table.print(value, iteration + 1, colon)
+                    elseif type(value) == 'string' then
+                    print(string.format('%' .. (iteration + 1) * 2 .. 's' .. key .. ': ' .. '[' .. type(value) .. '] \'' .. value .. '\'' .. colon, ''))
+                    elseif type(value) == 'number' then
+                    print(string.format('%' .. (iteration + 1) * 2 .. 's' .. key .. ': ' .. '[' .. type(value) .. '] ' .. value .. '' .. colon, ''))
+                    elseif type(value) == 'boolean' then
+                    print(string.format('%' .. (iteration + 1) * 2 .. 's' .. key .. ': ' .. '[' .. type(value) .. '] ' .. tostring(value) .. '' .. colon, ''))
+                    else
+                    print(string.format('%' .. (iteration + 1) * 2 .. 's' .. key .. ': ' .. '[' .. type(value) .. '] ' .. tostring(value) .. '' .. colon, ''))
+                    end
+                    count = count + 1
                 end
-                print( '}' )
+                
+                -- Print last `}`,
+                print(string.format('%' .. iteration * 2 .. 's}' .. t_colon, ''))
+                
             end
-        ";
+
     }
 }
